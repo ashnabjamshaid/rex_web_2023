579bd6f58ecd64cd271c9288d663c2df7a1c5d2a
ashnabjamshaid <ashnabjamshaid@gmail.com>
2022-12-26 17:10:31 +0500

    header update new page added

---
+++ js/spinner.js
@@ -0,0 +1,7 @@
+// (A) SHOW & HIDE SPINNER
+function show () {
+    document.getElementById("spinner").classList.add("show");
+  }
+  function hide () {
+    document.getElementById("spinner").classList.remove("show");
+  }
